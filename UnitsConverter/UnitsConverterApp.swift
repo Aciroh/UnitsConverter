1ba6517d0674688e18f36c30b712eaaaf55d12bc
Aciroh <horatiubolovan@gmail.com>
2022-08-25 14:43:47 +0300

    Initial Commit

---
+++ UnitsConverter/UnitsConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitsConverterApp.swift
+//  UnitsConverter
+//
+//  Created by Horatiu on 25.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct UnitsConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
